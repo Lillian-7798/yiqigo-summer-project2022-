dc8f7923c813e22cd34c87bc3605a91667b17f79
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-06-23 22:43:55 +0800

    注册界面代码

---
+++ YQGo/pages/Register/Register.vue
@@ -0,0 +1,101 @@
+<template>
+	<view class="content">
+		<view class="avatorWrapper">
+			<view class="avator">
+				<image class="img" src="../../static/user.webp" mode="widthFix"></image>
+			</view>
+		</view>
+		<view class="form">
+			<view class="inputWrapper">
+				<input class="input" type="text" value="" placeholder="请输入用户名"/>
+			</view>
+			<view class="inputWrapper">
+				<input class="input" type="password" value="" placeholder="请输入密码"/>
+			</view>
+			<view class="inputWrapper">
+				<input class="input" type="password" value="" placeholder="请再输入一次密码"/>
+			</view>
+			<view class="loginBtn">
+				<text class="btnValue">注册</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		background: #377EB4;
+		width: 100vw;
+		height: 100vh;
+	}
+	.avatorWrapper{
+		height: 30vh;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
+	}
+	.avator{
+		width: 200upx;
+		height: 200upx;
+		overflow: hidden;
+	}
+	.avator .img{
+			width: 100%
+		}
+	.form{
+		padding: 0 100upx;
+		margin-top: 80px;
+	}
+	.inputWrapper{
+		width: 100%;
+		height: 80upx;
+		background: white;
+		border-radius: 20px;
+		box-sizing: border-box;
+		padding: 0 20px;
+		margin-top: 25px;
+	}
+	.inputWrapper .input{
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		font-size: 15px;
+	}
+	.loginBtn{
+		width: 100%;
+		height: 80upx;
+		background: #77B307;
+		border-radius: 50upx;
+		margin-top: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
+	}
+	.loginBtn .btnValue{
+		color: white;
+	}
+	.registerBtn{
+			text-align: center;
+			color: #EAF6F9;
+			font-size: 15px;
+			margin-top: 20px;
+			}
+</style>
